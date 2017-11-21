b7cad1f4ffe9a740c16fa9a22fdacdd440b44c70
Unknown <Sudarat199858@gmail.com>
2017-11-21 17:38:06 +0700

    ข้อ1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,8 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+
+  cout << "Miss. " << "Sudarat Bunluea" << endl <<"ja" << endl <<"Female" << endl "December 18 1998" << endl "Hobbies sleep" << endl "Single";
+  cout <<
   return 0;
 }
